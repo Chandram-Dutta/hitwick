17d0810b4ff741c66faacd3adcb81368eadf3080
ChandramDutta <chandramdutta2004@gmail.com>
2023-11-15 02:42:57 +0530

    day 3 struggle

--- lib/presentation/game/components/hit.dart
+++ lib/presentation/game/components/hit.dart
@@ -7,17 +7,17 @@ import 'package:flutter/services.dart';
 
 class Hit extends RectangleComponent with KeyboardHandler {
   Hit({
-    required this.canvaseSize,
+    required this.canvasSize,
     required this.player,
   }) : super(
           position: player == Player.playerA
-              ? Vector2((canvaseSize.x - 48) / 2, 16)
-              : Vector2((canvaseSize.x - 48) / 2, canvaseSize.y - 32),
+              ? Vector2((canvasSize.x - 48) / 2, 16)
+              : Vector2((canvasSize.x - 48) / 2, canvasSize.y - 32),
           size: Vector2(64, 16),
           paint: Paint()..color = const Color(0xFFFFFFFF),
         );
 
-  final Vector2 canvaseSize;
+  final Vector2 canvasSize;
   final Player player;
 
   @override
@@ -37,17 +37,17 @@ class Hit extends RectangleComponent with KeyboardHandler {
         position.x -= 10;
       }
       if (keysPressed.contains(LogicalKeyboardKey.keyS) &&
-          position.y < canvaseSize.y / 2 - 100) {
+          position.y < canvasSize.y / 2 - 100) {
         position.y += 10;
       }
       if (keysPressed.contains(LogicalKeyboardKey.keyD) &&
-          position.x < canvaseSize.x - 64) {
+          position.x < canvasSize.x - 64) {
         position.x += 10;
       }
     }
     if (isKeyDown && player == Player.playerB) {
       if (keysPressed.contains(LogicalKeyboardKey.arrowUp) &&
-          position.y > canvaseSize.y / 2 + 100) {
+          position.y > canvasSize.y / 2 + 100) {
         position.y -= 10;
       }
       if (keysPressed.contains(LogicalKeyboardKey.arrowLeft) &&
@@ -55,11 +55,11 @@ class Hit extends RectangleComponent with KeyboardHandler {
         position.x -= 10;
       }
       if (keysPressed.contains(LogicalKeyboardKey.arrowDown) &&
-          position.y < canvaseSize.y - 16) {
+          position.y < canvasSize.y - 16) {
         position.y += 10;
       }
       if (keysPressed.contains(LogicalKeyboardKey.arrowRight) &&
-          position.x < canvaseSize.x - 64) {
+          position.x < canvasSize.x - 64) {
         position.x += 10;
       }
     }
